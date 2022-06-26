6f8f096c6cb30314a4b789b67869aa559b84f6d1
prado156 <ppattem@purdue.edu>
2022-06-26 07:48:59 +0530

    staying

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,7 +59,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Prahas Pattem.</h2>;
+  const two = <h2 className="big-heading">wefwef.</h2>;
   // const three = <h3 className="big-heading">a Computer Science Student at Purdue University.</h3>;
   const three = <h3 className="big-heading"> </h3>;
   const four = (
