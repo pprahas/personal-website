f9c26cef3cf62e395796c2588145f3698c5685c4
prado156 <ppattem@purdue.edu>
2022-07-21 12:20:11 +0530

    working on about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -152,14 +152,23 @@ const About = () => {
               2023.
             </p>
 
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a>an advertising agency</a>, <a href="https://starry.com/">a start-up</a>,{' '}
+            {/* <p>
+              Fast-forward to today, and I’ve had the privilege of working at <a>a start up </a>,{' '}
+              <a href="https://starry.com/">2 start-ups</a>,{' '}
               <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. You
+              can learn more about my technical experience <a href="/#jobs">here.</a> My main focus
+              these days is building accessible, inclusive products and digital experiences at{' '}
+              <a href="https://upstatement.com/">Upstatement</a> for a variety of clients.
+            </p> */}
+
+            <p>
+              I've had the privilege of working as a <a>Software Engineering Intern </a> at{' '}
+              <a>a startup </a> that builds Conversational AI for Customer Support and{' '}
+              <a>another startup</a> that develops Cyber-Physical Systems for railways.
+              Additionally, I have worked as a <a>Research and Development Intern</a> at a{' '}
+              <a> consultant company </a>. You can find out more about my technical experience{' '}
+              <a href="/#jobs">here.</a>
             </p>
 
             <p>
