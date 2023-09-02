274eb03a4e25ea990cb1f9551a9f38fdc21789a3
pprahas <p.prahas@protonmail.com>
2023-09-02 14:59:09 -0400

    23 to 24

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -211,7 +211,7 @@ const About = () => {
             </p> */}
             <p>
               I'm currently open to <a>New Grad Full-Time Sofware Engineer</a> positions starting{' '}
-              <a>Spring 2023. </a> If you think we would be a great match, I'd love to hear from
+              <a>Spring 2024. </a> If you think we would be a great match, I'd love to hear from
               you!
             </p>
             <StyledHeroSection>
